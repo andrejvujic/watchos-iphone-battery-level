61f4a847f60f11bcdffed067bfea1475a0e9e1e3
Andrej Vujić <vujicandrej365@gmail.com>
2022-08-05 12:04:53 +0200

    initial commit

--- Phone Battery Percentage WatchKit Extension/InterfaceController.swift
+++ Phone Battery Percentage WatchKit Extension/InterfaceController.swift
@@ -7,20 +7,51 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {}
+    
 
-class InterfaceController: WKInterfaceController {
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        if let deviceName = message["deviceName"] as? String {
+            self.deviceNameLabel.setText(deviceName)
+        }
+        
+        if let batteryLevel = message["batteryLevel"] as? String {
+            self.batteryLevelLabel.setText(batteryLevel)
+        }
+    }
+    
 
+    @IBOutlet weak var deviceNameLabel: WKInterfaceLabel!
+    @IBOutlet weak var batteryLevelLabel: WKInterfaceLabel!
+    
+    @IBAction func refreshButtonTapped() {
+        requestBatteryLevelFromPhone();
+    }
+    
+    var session = WCSession.default
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
+        session.delegate = self
+        session.activate()
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
+        requestBatteryLevelFromPhone()
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
     }
 
+    func requestBatteryLevelFromPhone() {
+        deviceNameLabel.setText("iPhone")
+        batteryLevelLabel.setText("...")
+
+        let dataToPhone: [String: Any] = ["requestFromWatch": "batteryLevelRequest" as Any]
+        session.sendMessage(dataToPhone, replyHandler: nil, errorHandler: nil)
+    }
 }
--- Phone Battery Percentage/ViewController.swift
+++ Phone Battery Percentage/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {}
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {}
+    
+    func sessionDidDeactivate(_ session: WCSession) {}
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        if let dataFromWatch = message["requestFromWatch"] as? String {
+            print(dataFromWatch)
+            if dataFromWatch == "batteryLevelRequest" {
+                sendBatteryLevelToWatch()
+            }
+        }
+    }
+    
+    var session: WCSession?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupWCSession()
+        setupBatteryLevelMonitoring()
     }
-
-
+    
+    func setupWCSession() {
+        if WCSession.isSupported() {
+            session = WCSession.default
+            session?.delegate = self
+            session?.activate()
+        }
+    }
+    
+    func setupBatteryLevelMonitoring() {
+        UIDevice.current.isBatteryMonitoringEnabled = true
+    }
+    
+    var batteryLevel: Float { UIDevice.current.batteryLevel }
+    var deviceName: String { UIDevice.current.name }
+    
+    func sendBatteryLevelToWatch() {
+        if let s = self.session, s.isReachable {
+            let dataToWatch: [String: Any] =  ["batteryLevel": "\(batteryLevel * 100)%", "deviceName": deviceName,
+            ]
+            s.sendMessage(dataToWatch, replyHandler: nil, errorHandler: nil)
+        }
+    }
+    
 }
 
